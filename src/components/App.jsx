cc476c77c3603f60273d0c453a918146f94f75ea
Yuriy-Dobryi <yuriy.dobryi@gmail.com>
2023-06-06 18:33:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,7 @@ import { ImageGallery } from "./ImageGallery/ImageGallery";
 export class App extends Component {
   state = {
     imageName: '',
+    page: 1,
   }
 
   componentDidMount() {
@@ -17,6 +18,33 @@ export class App extends Component {
 
   changeImageName = (newName) => {
     this.setState({ imageName: newName });
+  }
+
+  async getData() {
+    const config = {
+      params: {
+        key: `35543828-6c73cc5fdea5a14873063547d`,
+        q: this.query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        per_page: 40,
+      },
+    };
+    const URL = `https://pixabay.com/api/`;
+
+    const response = await axios.get(URL, config);
+    this.incrementPage();
+    return response.data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
   
   render() {
